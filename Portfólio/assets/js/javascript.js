b471899589eed9a12eacf87054ec7f03aa2a96ce
brunoks <brunokasv@hotmail.com>
2018-05-06 16:48:18 -0300

    Apenas ajustes

--- Portfólio/assets/js/javascript.js
+++ Portfólio/assets/js/javascript.js
@@ -1,29 +1,12 @@
 
-function newPopup(){
-varWindow = window.open (
-'popup.html',
-'pagina',
-"width=350, height=255, top=100, left=110, scrollbars=no " );
+
+
+
+
+function closed(el) {
+        document.getElementById(el).style.display = 'none';
 }
 
-function abrir(URL) {
-
-  var width = 150;
-  var height = 250;
-
-  var left = 99;
-  var top = 99;
-
-  window.open(URL,'janela', 'width='+width+', height='+height+', top='+top+', left='+left+', scrollbars=yes, status=no, toolbar=no, location=no, directories=no, menubar=no, resizable=no, fullscreen=no');
-
+function show(el) {
+    document.getElementById(el).style.display = 'block';
 }
-
-
-function Mudarestado(el) {
-        var display = document.getElementById(el).style.display;
-
-        if(display == "block")
-            document.getElementById(el).style.display = 'none';
-        else
-            document.getElementById(el).style.display = 'block';
-    }
