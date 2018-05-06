b9f73619abda789fe6197fcf81a92090a0353921
brunoks <brunokasv@hotmail.com>
2018-05-05 22:29:12 -0300

    Finalmente conseguir fazer o popup

--- Portfólio/assets/js/javascript.js
+++ Portfólio/assets/js/javascript.js
@@ -17,17 +17,13 @@ function abrir(URL) {
   window.open(URL,'janela', 'width='+width+', height='+height+', top='+top+', left='+left+', scrollbars=yes, status=no, toolbar=no, location=no, directories=no, menubar=no, resizable=no, fullscreen=no');
 
 }
-function ocultar(el){
 
-document.getElementById(el).style.display = 'none';
-
-}
 
 function Mudarestado(el) {
         var display = document.getElementById(el).style.display;
 
-        if(display == "none")
-            document.getElementById(el).style.display = 'block';
-        else
+        if(display == "block")
             document.getElementById(el).style.display = 'none';
+        else
+            document.getElementById(el).style.display = 'block';
     }
